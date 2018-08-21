d00c1b8273181f94810d6c3f609ee570fe185098
Enrico Nardelli <enriconardelli@users.noreply.github.com>
2018-08-21 10:39:20 +0200

    first trial

---
+++ my_code/plot_trial.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 19 14:35:08 2018
+
+@author: Enrico Nardelli
+"""
+
+import thinkstats2.py
+import thinkplot
+
+def trialPlot():
+    
+def main():
+    trialPlot()
+
+if __name__ == "__main__":
+    main()
